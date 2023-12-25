af99bcd501853f90de996f84af09be50a49e9687
Ney Júnior <neyjunior.n@gmail.com>
2023-12-25 19:48:23 -0300

    commit

---
+++ frontend/app/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Login from './components/login';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <Login />
+      </header>
+    </div>
+  );
+}
+
+export default App;
